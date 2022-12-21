d8d9c649b5cfbb58095abf6b7fcc03afbeebc830
yraffic <yraffic@gmail.com>
2022-12-20 23:37:29 -0300

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "9cfdbdbd4696d014dc375cbe3d17cb8d"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
